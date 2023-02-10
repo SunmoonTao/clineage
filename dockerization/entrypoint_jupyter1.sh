6b286523c26096e80555ca2b47e17db3630b51aa
SunmoonTao <taoliming.too@gmail.com>
2023-02-09 18:23:15 -0800

    move jupyter entrypoint to resources

--- dockerization/entrypoint_jupyter1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-. $(which activate) py36
-jupyter notebook --allow-root --ip=0.0.0.0
---
+++ dockerization/resources/entrypoint_jupyter1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+. $(which activate) py36
+yes | pip install jupyter==1.0.0
+cp /clineage/dockerization/resources/local_settings.py /clineage/clineage
+cd /
+jupyter notebook --allow-root --ip=0.0.0.0
