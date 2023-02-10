1f2078267a542e701ec9f15dd9ccdf9db59bd4f6
SunmoonTao <taoliming.too@gmail.com>
2023-02-09 20:57:34 -0800

    jupyter installed by requirement-dev

--- dockerization/resources/entrypoint_jupyter1.sh
+++ dockerization/resources/entrypoint_jupyter1.sh
@@ -2,7 +2,7 @@
 set -e
 
 . $(which activate) py36
-yes | pip install jupyter==1.0.0
+# yes | pip install jupyter==1.0.0
 cp /clineage/dockerization/resources/local_settings.py /clineage/clineage
 cd /
 jupyter notebook --allow-root --ip=0.0.0.0
